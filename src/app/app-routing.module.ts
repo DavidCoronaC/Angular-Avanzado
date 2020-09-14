1296b5f67872017d81f6a4625462be8dcaf58b35
DavidCoronaC <darkes13@hotmail.com>
2020-09-14 11:13:13 -0500

    Se creo la estructuctura completa de la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NotpagesfoundComponent } from './pages/notpagesfound/notpagesfound.component';
+import { PagesComponent } from './pages/pages.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'Grafica1', component: Grafica1Component },
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+    ]
+  },
+  
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  
+  { path: '**', component: NotpagesfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
